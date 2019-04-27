34a23c4877e3d08ce03550017e2617b48aa90581
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-04-27 15:11:36 +0530

    Update matcher to work with new data format

--- src/match.js
+++ src/match.js
@@ -8,7 +8,7 @@ import tamil from './data/tamil.json'
 const scriptData = { malayalam, tamil }
 const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length
 const sortByScore = (a, b) => parseFloat(b.score) - parseFloat(a.score)
-const simpifyStrokes = (strokes) => strokes.map((stroke) => simplify(stroke, 5, false))
+const simpifyStrokes = (strokes) => strokes.map((stroke) => simplify(stroke, 5, true))
 
 export default class Match {
   constructor (script, threshold) {
@@ -17,36 +17,39 @@ export default class Match {
   }
 
   run (strokes) {
-    let candiates = []
+    let candidates = []
 
-    if (!strokes) return candiates
+    if (!strokes) return candidates
 
     strokes = Array.isArray(strokes[0]) ? strokes : [strokes]
 
     // Simplify the curve with 5px tolerance
     let simplifiedStrokes = simpifyStrokes(strokes)
-    console.log(JSON.stringify({ 'strokes': simplifiedStrokes }))
 
     for (let key in this.data) {
       let score = this.matchLetter(
         simplifiedStrokes,
-        this.data[key].strokes,
-        this.data[key].variants
+        this.data[key].samples
       )
       if (score) {
-        candiates.push({ pattern: key, score })
+        candidates.push({ pattern: key, score })
+        // Good score. Finish here
+        if (score > 0.90) { break }
       }
     }
-
     // Sort by descending order of scores
-    return candiates.sort(sortByScore)
+    return candidates.sort(sortByScore)
   }
 
-  matchLetter (letterData, candidateStrokes, variants) {
-    if (letterData.length === candidateStrokes.length) {
+  matchLetter (letterData, samples) {
+    for (let i = 0; i < samples.length; i++) {
+      let sample = samples[i]
+      if (letterData.length !== sample.strokes.length) {
+        continue
+      }
       let strokeScores = []
       let matching = letterData.every((stroke, index) => {
-        let score = this.match(stroke, candidateStrokes[index])
+        let score = this.match(stroke, sample.strokes[index])
         if (score >= this.threshold) {
           strokeScores.push(score)
           return true
@@ -55,13 +58,6 @@ export default class Match {
       if (matching) {
         // return average score
         return average(strokeScores)
-      }
-    }
-
-    if (variants) {
-      for (let i = 0; i < variants.length; i++) {
-        let score = this.matchLetter(letterData, variants[i].strokes)
-        if (score) return score
       }
     }
 
