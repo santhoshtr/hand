5776bd4603e42212d67890d70492fabf9c2dc70c
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-05-10 11:22:11 +0530

    Fix linting issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   module: {
     rules: [
       {
         test: /\.worker\.js$/,
         use: {
-          loader: 'worker-loader',
+          loader: "worker-loader",
           options: { inline: true }
         }
       }
     ]
   },
   output: {
-    library: 'HandwritingRecognition',
-    libraryExport: 'default',
-    libraryTarget: 'umd',
-    filename: 'handwriting-recognition.js',
-    path: path.resolve(__dirname, 'dist')
+    library: "HandwritingRecognition",
+    libraryExport: "default",
+    libraryTarget: "umd",
+    filename: "handwriting-recognition.js",
+    path: path.resolve(__dirname, "dist")
   },
-  devtool: 'source-map'
-}
+  devtool: "source-map"
+};
